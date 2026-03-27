cf406f960cc9a2259defcbba75f80490d4b0f475
boxxy <91877601+hanifakbari@users.noreply.github.com>
2026-03-27 16:55:14 +0700

    fix(jest): exclude Playwright tests from Jest test runner

--- jest.config.ts
+++ jest.config.ts
@@ -7,6 +7,8 @@ const config: Config = {
   coverageProvider: "v8",
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  testMatch: ["**/__tests__/**/*.{ts,tsx}", "**/*.test.{ts,tsx}"],
+  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/tests/"],
   moduleNameMapper: {
     "^@/assets(.*)$": "<rootDir>/src/assets$1",
     "^@/components(.*)$": "<rootDir>/src/components$1",
