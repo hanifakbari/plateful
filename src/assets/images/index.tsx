55f458f1a7e063be8f9fcd45eb33bf8ac029fac7
boxxy <91877601+hanifakbari@users.noreply.github.com>
2026-03-27 11:47:56 +0700

    chore: add logo plateful, app mockup, and store icon assets

--- src/assets/images/index.tsx
+++ src/assets/images/index.tsx
@@ -1,3 +1,6 @@
+export { default as Plate } from "./plate-icon.svg";
+export { default as Plateful } from "./plateful-icon.svg";
+
 export { default as Hero } from "./hero-image.webp";
 
 export { default as Food1 } from "./food-image-01.webp";
@@ -9,3 +12,7 @@ export { default as Food6 } from "./food-image-06.webp";
 export { default as Food7 } from "./food-image-07.webp";
 export { default as Food8 } from "./food-image-08.webp";
 export { default as Food9 } from "./food-image-08.webp";
+
+export { default as PlayStore } from "./playStore-icon.svg";
+export { default as AppStore } from "./appStore-icon.svg";
+export { default as AppMockup } from "./appMockup-image.png";
