ce085ac3403cae525c1ed31dc7da73c1290a20a8
boxxy <91877601+hanifakbari@users.noreply.github.com>
2026-03-27 09:16:15 +0700

    chore: add assets for why plateful section

--- src/assets/images/index.tsx
+++ src/assets/images/index.tsx
@@ -1,5 +1,11 @@
-export { default as Hero } from "./hero-image.jpeg";
+export { default as Hero } from "./hero-image.webp";
 
-export { default as Food1 } from "./food-image-01.jpeg";
-export { default as Food2 } from "./food-image-02.jpeg";
-export { default as Food3 } from "./food-image-03.jpeg";
+export { default as Food1 } from "./food-image-01.webp";
+export { default as Food2 } from "./food-image-02.webp";
+export { default as Food3 } from "./food-image-03.webp";
+export { default as Food4 } from "./food-image-04.webp";
+export { default as Food5 } from "./food-image-05.webp";
+export { default as Food6 } from "./food-image-06.webp";
+export { default as Food7 } from "./food-image-07.webp";
+export { default as Food8 } from "./food-image-08.webp";
+export { default as Food9 } from "./food-image-08.webp";
